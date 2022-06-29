daf23446cadfb4d77e83d08f5f02780c01058bfd
Vadim Markov <vadim_markov@epam.com>
2022-06-30 01:15:06 +0200

    Initial

---
+++ src/main/java/edu/epam/fop/lambdas/IntArrayReducers.java
@@ -0,0 +1,20 @@
+package edu.epam.fop.lambdas;
+
+// TODO write your implementation here
+public interface IntArrayReducers {
+
+  static IntArrayReducer SUMMARIZER = null;
+
+  static IntArrayReducer MULTIPLIER = null;
+
+  static IntArrayReducer MIN_FINDER = null;
+
+  static IntArrayReducer MAX_FINDER = null;
+
+  static IntArrayReducer AVERAGE_CALCULATOR = null;
+
+  static IntArrayReducer UNIQUE_COUNTER = null;
+
+  static IntArrayReducer SORT_DIRECTION_DEFINER = null;
+
+}
